6daf3afab6d3be516a8c20163e1efd8504a94f19
Your Name <ayandamantshinga30@gmail.com>
2024-10-17 08:50:18 +0000

    uploaded files

---
+++ EmployeeManager/Models/DepartmentAttribute.cs
@@ -0,0 +1,42 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManager.Models
+{
+    /*
+    an attribute can be used anywhere so to restrict that, developer can actually only use it on a method or a property or a class
+    So in order to restrict the usage of a attribute we use USAGE ATTRIBUTE which governing how attributes work inception
+    */
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+
+    internal class DepartmentAttribute : ValidationAttribute
+    {
+        private readonly string[] _allowedDepartments;
+
+        public DepartmentAttribute(string[] allowedDepartments)
+        {
+            _allowedDepartments = allowedDepartments;
+        }
+
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            //CHECK IF THE VALUE IS NULL OR EMPTY
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return new ValidationResult("Department is required.");
+            }
+
+            //check if the provided department is in the list of allowed departments
+            var departmentValue = value.ToString();
+
+            //look at each department name and see if it matches the name recieved as input 
+            if (Array.Exists(_allowedDepartments, dept => dept.Equals(departmentValue, StringComparison.OrdinalIgnoreCase)))
+            {
+                return ValidationResult.Success;
+            }
+
+            //Return error if the department is not valid
+            return new ValidationResult("Ïnvalid department");
+        }
+    }
+}
---
+++ EmployeeManager/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EmployeeManager.Models
+{
+    public class Employee
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [MaxLength(100,ErrorMessage ="First name characters cannot be more that 100 character")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string? Email { get; set; }
+    
+        [Required(ErrorMessage = "Department is required")]
+        [Department(new[] { "IT","HR" })]//Allowed departments
+        [MaxLength(50, ErrorMessage ="Department name cannot be more than 50 characters")]
+        public string? Department { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        [DataType(DataType.Date)]// Specifies that this is a date
+        public DateTime? HireDate { get; set; }
+
+
+    }
+}
